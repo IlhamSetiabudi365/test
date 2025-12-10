8d7744514e852c6646bd73a1d2cbb60e7760e1a3
IlhamSetiabudi365 <24204166@siswa.um.edu.my>
2025-12-10 15:59:24 +0800

    also test

--- test.java
+++ test.java
@@ -1,6 +1,5 @@
 public class test{
     public static void main(String[] args){
-        System.out.println("this is a test");   
-        System.out.println("this is test 2");
+        System.out.println("this is a test");
     }
 }
